a1197e75178103144bb56790ef7c8bacf3cd263d
Christian Kube <christian.kube@web.de>
2022-05-28 21:54:05 +0200

    SCOPES

--- app/app.js
+++ app/app.js
@@ -49,7 +49,7 @@ app.get('/login', function (req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-read-playback-state user-modify-playback-state';
+  var scope = 'user-read-private user-read-email user-read-playback-state user-modify-playback-state playlist-modify-public';
   console.log(querystring.stringify({
     response_type: 'code',
     client_id: client_id,
